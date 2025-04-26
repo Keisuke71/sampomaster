254046869259c13f77be3b987185e2d5e9dbde20
松本 圭祐 <kmatsumoto071@matsumotos-MacBook-Air.local>
2025-04-26 11:30:34 +0900

    Initial commit

---
+++ sampomaster/sampomasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampomasterApp.swift
+//  sampomaster
+//
+//  Created by 松本 圭祐 on 2025/04/26.
+//
+
+import SwiftUI
+
+@main
+struct sampomasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sampomasterTests/sampomasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  sampomasterTests.swift
+//  sampomasterTests
+//
+//  Created by 松本 圭祐 on 2025/04/26.
+//
+
+import Testing
+@testable import sampomaster
+
+struct sampomasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
